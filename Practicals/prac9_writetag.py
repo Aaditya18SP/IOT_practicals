bbf27d9e0068cdee6d0f093069119dc1e80a8b11
Aaditya18SP <aaditya.sp@somaiya.edu>
2023-09-16 00:05:47 +0530

    steps of RFID

--- Practicals/prac9_writetag.py
+++ Practicals/prac9_writetag.py
@@ -4,7 +4,7 @@ from mfrc522 import SimpleMFRC522
 reader=SimpleMFRC522()
 
 try:
-    text=input("Enter the data to write on a tag")
+    text=input("Enter the data to write on a tag: ")
     print("Print your tag to write...")
     reader.write(text)
     print("Data written successfully")
