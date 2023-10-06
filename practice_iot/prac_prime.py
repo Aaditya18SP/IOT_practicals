42828258eafe2ebbc261f40e323fd28362002df4
AadityaPal <aaditya.sp@somaiya.edu>
2023-10-06 09:39:39 +0530

    prime numbers done

--- practice_iot/prac_prime.py
+++ practice_iot/prac_prime.py
@@ -8,20 +8,19 @@ display.SetBrightnes(1)
 prime_numbers=list()
 
 def prime_numbers_chk():
-    f=0
-    for i in range(100):
-        
+    for i in range(2,1020):
+        f=0
         for j in range (2,i):
-            if(i%2==0):
+            if(i%j==0):
                 f=1
                 break
-            if(f==1):
-                prime_numbers.append(i)
+        if(f==0):
+            prime_numbers.append(i)
 
 
 def displayNumbers():
     prime_numbers_chk()
-     for i in prime_numbers:
+    for i in prime_numbers:
         # convert the int to string  
         num_str= str(i)
         
@@ -36,7 +35,7 @@ def displayNumbers():
         #separate each digit from the string
         num_to_display=[int(zero_str[0]),int(zero_str[1]),int(zero_str[2]),int(zero_str[3])]
         display.Show(num_to_display)
-        time.sleep(1)
+        time.sleep(0.1)
 
 def addLeadingZeros(var):
     zeros_str=""
